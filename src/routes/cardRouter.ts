ccf2c7e46d60dcbe0a474fa3f8ef88a2656d18bf
Eduardo Nagashima <eduyudji@gmail.com>
2022-07-12 00:36:12 -0300

    unlock card done

--- src/routes/cardRouter.ts
+++ src/routes/cardRouter.ts
@@ -4,13 +4,14 @@ import activateCard from "../controllers/activateCardController.js";
 import newCard from "../controllers/newCardController.js";
 import getBalance from "../controllers/balanceController.js";
 import lockCard from "../controllers/lockCardController.js";
+import unlockCard from "../controllers/unlockCardController.js";
 
 const cardRouter = Router();
 cardRouter.post('/card/new', newCardValidation, newCard);
 cardRouter.post('/card/activate', activateCardValidation, activateCard);
 //FIXME FAZER O BALANCO
 cardRouter.get('/card/:id/balance', cardValidationById, getBalance);
-//AINDA FALTA TERMINAR
-cardRouter.post('/card/:id/lock', cardValidationById, lockCard)
+cardRouter.post('/card/:id/lock', cardValidationById, lockCard);
+cardRouter.post('/card/:id/unlock', cardValidationById, unlockCard);
 
 export default cardRouter;
